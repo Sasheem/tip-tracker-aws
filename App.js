b13bddefae1f2aeeec6be19d0de7b076f807641a
Sasheem <sts12@me.com>
2020-11-05 17:01:38 -0500

    set up navigation component

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-
 Amplify.configure(config);
+
+function HomeScreen() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Home Screen</Text>
+		</View>
+	);
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Hello world, my new app</Text>
-			<StatusBar style='auto' />
-		</View>
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name='Home' component={HomeScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
 
