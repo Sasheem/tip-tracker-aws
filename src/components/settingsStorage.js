29061b93026fb37c1095e1ffa8e9c20260007438
Sasheem Snell <sasheem.snell@gmail.com>
2021-06-02 14:37:47 -0400

    changed naming and removed unused code

--- src/components/settingsStorage.js
+++ src/components/settingsStorage.js
@@ -1,18 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
-// import DocumentPicker from 'react-native-document-picker';
-// import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 // local components
-import Input from '../components/common/input';
-import Button from '../components/common/button';
+import CustomInput from './common/customInput';
+import Button from './common/customButton';
 
 const SettingsStorage = () => {
     const [exportEmail, setExportEmail] = useState('');
     const [exportError, setExportError] = useState('');
-    // const [importFile, setImportFile] = useState('');
-    // const [importError, setImportError] = useState('');
-
+    const [importError, setImportError] = useState('');
 
     // controlled form functions
     const onExportChange = text => {
@@ -22,35 +18,11 @@ const SettingsStorage = () => {
     const sendExport = () => {
         console.log(`attempting to email export data`);
     }
-    const onSyncPress = () => {
-        console.log(`sync cloud data with local DataStore`);
-    }
 
-    // const onImportPress = async () => {
-    //     // Pick a single file
-    //     try {
-    //         const res = await DocumentPicker.pick({
-    //         type: [DocumentPicker.types.images],
-    //         });
-    //         console.log(
-    //         res.uri,
-    //         res.type, // mime type
-    //         res.name,
-    //         res.size
-    //         );
-    //     } catch (err) {
-    //         if (DocumentPicker.isCancel(err)) {
-    //         // User cancelled the picker, exit any dialogs or menus and move on
-    //         } else {
-    //         throw err;
-    //         }
-    //     }
-    // }
-    
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Storage Options</Text>
-            <Input 
+            <CustomInput 
                 label='Export data'
                 desc='Enter email to send your shift data to.'
                 placeholder='Email address'
@@ -67,33 +39,6 @@ const SettingsStorage = () => {
                 <Button label='Sync' onPress={onSyncPress} customStyle={{ alignSelf: `flex-end` }} />
             </View>
             
-            {/* <View style={styles.row}>
-                <SafeAreaView style={styles.importContainer}>
-                    <Text>Import data</Text>
-                    <View style={styles.import}>
-                        <TouchableOpacity
-                            activeOpacity={0.5}
-                            style={styles.importButton}
-                            onPress={onImportPress}
-                        >
-                            <Text>Click here to upload a file</Text>
-                            <MaterialCommunityIcons name="import" size={24} color="black" />
-                        </TouchableOpacity>
-                    </View>
-                    {importFile !== '' && <View style={styles.import}>
-                        <Text style={styles.textStyle}>
-                            File Name: {importFile.name ? importFile.name : ''}
-                            {'\n'}
-                            Type: {importFile.type ? importFile.type : ''}
-                            {'\n'}
-                            File Size: {importFile.size ? importFile.size : ''}
-                            {'\n'}
-                            URI: {importFile.uri ? singleFile.uri : ''}
-                            {'\n'}
-                            </Text>
-                    </View>}
-                </SafeAreaView>
-            </View> */}
             <View style={styles.fillerLg} />
         </View>
     );
