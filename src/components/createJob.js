3816360e7aaa899bb7553135848c7c645338205a
Sasheem Snell <sasheem.snell@gmail.com>
2021-06-09 16:36:53 -0400

    changed state to context and utitlize reusable form

--- src/components/createJob.js
+++ src/components/createJob.js
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import {
-	Platform,
 	StyleSheet,
 	View,
-	Text,
-	TextInput,
 	TouchableOpacity,
-	KeyboardAvoidingView,
-	Keyboard,
-	TouchableWithoutFeedback,
 } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
-import { ScrollView } from 'react-native-gesture-handler';
-import { API, graphqlOperation } from 'aws-amplify';
 
-import { createJob } from '../graphql/mutations';
+
+import { Context as JobsContext } from '../context/JobsContext';
+import FormJob from './formJob';
 
 /**
  *
@@ -29,92 +23,8 @@ import { createJob } from '../graphql/mutations';
  */
 
 const CreateJob = ({ navigation }) => {
-	const [jobTitle, setJobTitle] = useState('');
-	const [jobWage, setJobWage] = useState('');
-	const [storeName, setStoreName] = useState('');
-	const [storeAddress, setStoreAddress] = useState({
-		city: '',
-		country: 'USA',
-		address_line1: '',
-		address_line2: '',
-		address_state: '',
-		address_zip: '',
-	});
-	const [formError, setFormError] = useState('');
-
-	const onTitleChange = (text) => {
-		setFormError('');
-		setJobTitle(text);
-	};
-
-	const onWageChange = (text) => {
-		setFormError('');
-		setJobWage(text);
-	};
-
-	const onNameChange = (text) => {
-		setFormError('');
-		setStoreName(text);
-	};
-
-	const onAddressChange = (event, name) => {
-		setStoreAddress({ ...storeAddress, [name]: event.nativeEvent.text });
-	};
-
-	const handleSubmit = () => {
-		const { city, address_line1, address_state, address_zip } = storeAddress;
-
-		// job title and wage are both required
-		if (jobTitle === '') {
-			return setFormError('Fill in job title');
-		}
-		if (jobWage === '') {
-			return setFormError('Fill in hourly wage');
-		} else if (parseFloat(jobWage) > 30) {
-			return setFormError('Wage is too high');
-		}
-
-		// if line one is provided, check all other fields
-		if (address_line1 !== '') {
-			if (address_line1 === '') {
-				return setFormError('Fill in Address Line 1');
-			}
-			if (address_state === '') {
-				return setFormError('Fill in State');
-			}
-			if (address_zip === '') {
-				return setFormError('Fill in Zip Code');
-			}
-			if (city === '') {
-				return setFormError('Fill in City');
-			}
-		}
-
-		// prepare data
-		const input = {
-			jobTitle,
-			jobWage: parseFloat(jobWage),
-			storeName,
-			storeAddress,
-		};
-
-		// write to backend
-		API.graphql(graphqlOperation(createJob, { input }));
-		setJobTitle('');
-		setJobWage('');
-		setStoreName('');
-		setStoreAddress({
-			city: '',
-			country: 'USA',
-			address_line1: '',
-			address_line2: '',
-			address_state: '',
-			address_zip: '',
-		});
-
-		// navigate back to parent screen
-		navigation.navigate('CurrentJobs');
-	};
+	// context
+	const { addJob } = useContext(JobsContext); 
 
 	return (
 		<View style={styles.container}>
@@ -123,155 +33,44 @@ const CreateJob = ({ navigation }) => {
 					<AntDesign name='close' size={32} color='black' />
 				</TouchableOpacity>
 			</View>
-			<KeyboardAvoidingView
-				behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
-				style={styles.container}
-			>
-				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-					<ScrollView>
-						<View style={styles.inner}>
-							<Text style={styles.title}>Create Job</Text>
-
-							{/* Job Title Input */}
-							<View style={styles.row}>
-								<Text style={styles.subtitle}>Job Title</Text>
-								<TextInput
-									style={styles.textInput}
-									placeholder='Server, Bartender, etc.'
-									onChangeText={(text) => onTitleChange(text)}
-									value={jobTitle}
-								/>
-							</View>
-
-							{/* Job Name Input */}
-							<View style={styles.row}>
-								<Text style={styles.subtitle}>Job Name</Text>
-								<TextInput
-									style={styles.textInput}
-									placeholder='Outback, Carrabas, etc.'
-									onChangeText={(text) => onNameChange(text)}
-									value={storeName}
-								/>
-							</View>
-
-							{/* Job Wage Input */}
-							<View style={styles.row}>
-								<Text style={styles.subtitle}>Job Wage</Text>
-								<TextInput
-									style={styles.textInput}
-									placeholder='5.54'
-									keyboardType='decimal-pad'
-									returnKeyType='done'
-									onChangeText={(text) => onWageChange(text)}
-									value={jobWage}
-								/>
-							</View>
-
-							<View style={styles.rowLg}>
-								<Text style={styles.subtitle}>Job Address</Text>
-								<TextInput
-									style={styles.textInput}
-									placeholder='Address Line 1'
-									onChange={(event) => onAddressChange(event, 'address_line1')}
-									value={storeAddress.address_line1}
-								/>
-								<TextInput
-									style={styles.textInput}
-									placeholder='Address Line 2'
-									onChange={(event) => onAddressChange(event, 'address_line2')}
-									value={storeAddress.address_line2}
-								/>
-								<TextInput
-									style={styles.textInput}
-									placeholder='City'
-									onChange={(event) => onAddressChange(event, 'city')}
-									value={storeAddress.city}
-								/>
-								<TextInput
-									style={styles.textInput}
-									placeholder='Zip Code'
-									keyboardType='number-pad'
-									returnKeyType='done'
-									onChange={(event) => onAddressChange(event, 'address_zip')}
-									value={storeAddress.address_zip}
-								/>
-								<TextInput
-									style={styles.textInput}
-									placeholder='State'
-									onChange={(event) => onAddressChange(event, 'address_state')}
-									value={storeAddress.address_state}
-								/>
-							</View>
-							<View style={styles.btnContainer}>
-								<TouchableOpacity onPress={handleSubmit} style={styles.btn}>
-									<Text style={styles.btnText}>Save</Text>
-								</TouchableOpacity>
-							</View>
-							{formError !== '' && (
-								<Text style={{ color: `red` }}>{formError}</Text>
-							)}
-							<View style={styles.hardFiller} />
-						</View>
-					</ScrollView>
-				</TouchableWithoutFeedback>
-			</KeyboardAvoidingView>
+			<FormJob 
+				labels={{
+					title: `Add a job`,
+					button: `Add`
+				}}
+				initFormValue={{
+					jobTitle: ``,
+					jobWage: ``,
+					storeName: ``,
+					storeAddress: {
+						city: '',
+						country: 'USA',
+						address_line1: '',
+						address_line2: '',
+						address_state: '',
+						address_zip: '',
+					}
+				}}
+				onSubmit={(title, wage, name, address) => {
+					addJob(title, wage, name, address, () => navigation.navigate('CurrentJobs'))
+				}}
+				isEdit={false}
+			/>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	hardFiller: {
-		height: Platform.OS === 'ios' ? 300 : 200,
-	},
 	container: {
 		flex: 1,
 		justifyContent: `flex-start`,
 		padding: 12,
-	},
-	inner: {
-		flex: 1,
 	},
 	header: {
 		flex: 0.1,
 		width: `100%`,
 		justifyContent: `center`,
 		alignItems: `flex-start`,
-	},
-	row: {
-		flex: 1,
-	},
-	rowLg: {
-		flex: 3,
-	},
-	btnContainer: {
-		marginTop: 12,
-		flex: 1,
-	},
-	btn: {
-		padding: 10,
-		backgroundColor: `#06D6A0`,
-		alignItems: `center`,
-		borderRadius: 2.5,
-	},
-	btnText: {
-		color: `white`,
-		fontSize: 18,
-		fontWeight: `500`
-	},
-	title: {
-		fontSize: 36,
-		marginBottom: 24,
-	},
-	subtitle: {
-		fontSize: 16,
-		fontWeight: `bold`,
-		paddingBottom: 5,
-	},
-	textInput: {
-		height: 40,
-		borderColor: '#000000',
-		borderBottomWidth: 1,
-		marginBottom: 36,
 	},
 });
 
--- src/components/editJob.js
+++ src/components/editJob.js
@@ -1,24 +1,20 @@
 import React, { useState, useEffect, useContext } from 'react';
 import {
 	View,
-	Text,
 	StyleSheet,
-	TextInput,
 	TouchableOpacity,
-	KeyboardAvoidingView,
-	Keyboard,
-	TouchableWithoutFeedback,
-	Platform,
 } from 'react-native';
 import _ from 'lodash';
-import { ScrollView } from 'react-native-gesture-handler';
 import { Ionicons } from '@expo/vector-icons';
 import { Context as JobsContext } from '../context/JobsContext';
+import FormJob from './formJob';
 
 const EditJob = ({ route, navigation }) => {
 	// context
-	const { removeJob } = useContext(JobsContext);
+	const { state, editJob, removeJob } = useContext(JobsContext);
 	// state
+	const id = route.params.job.id;
+	const jobItem = state.find((item) => item.id === id);
 	const [data, setData] = useState({
 		id: '',
 		jobTitle: '',
@@ -33,22 +29,6 @@ const EditJob = ({ route, navigation }) => {
 			country: '',
 		},
 	});
-	const [newData, setNewData] = useState({
-		jobTitle: '',
-		jobWage: '',
-		storeName: '',
-	});
-	const [newStoreAddress, setNewStoreAddress] = useState({
-		storeAddress: {
-			address_line1: '',
-			address_line2: '',
-			address_state: '',
-			city: '',
-			zip: '',
-			country: '',
-		},
-	});
-	const [formError, setFormError] = useState('');
 
 	// set up form with incoming route data
 	useEffect(() => {
@@ -64,11 +44,6 @@ const EditJob = ({ route, navigation }) => {
 		}
 	}, [route.params]);
 
-	// handle submit button
-	const handleSubmit = () => {
-		console.log(`editJob submit button pressed`);
-	};
-
 	return (
 		<View style={styles.container}>
 			<View style={styles.header}>
@@ -76,152 +51,37 @@ const EditJob = ({ route, navigation }) => {
 					<Ionicons name='md-close' size={32} color='black' />
 				</TouchableOpacity>
 			</View>
-			<KeyboardAvoidingView
-				behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
-				style={styles.container}
-			>
-				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-					<ScrollView style={styles.body}>
-						<View style={styles.titleRow}>
-							<Text style={styles.title}>Edit Job</Text>
-							<TouchableOpacity 
-								style={styles.deleteBtn} 
-								onPress={() => removeJob(data.id, () => navigation.navigate('CurrentJobs'))}
-							>
-								<Ionicons name="md-trash" size={24} color="#EF476F" />
-								<Text style={styles.deleteText}>Delete</Text>
-							</TouchableOpacity>
-						</View>
-						
-						<View style={styles.row}>
-							<Text style={styles.subtitle}>Title</Text>
-							<TextInput
-								style={styles.input}
-								placeholder={data.jobTitle}
-								value={newData.jobTitle}
-								onChangeText={(text) =>
-									setNewData({ ...newData, jobTitle: text })
-								}
-							/>
-						</View>
-						<View style={styles.row}>
-							<Text style={styles.subtitle}>Wage</Text>
-							<TextInput
-								style={styles.input}
-								placeholder={`$${data.jobWage.toString()}.00`}
-								keyboardType='decimal-pad'
-								value={newData.jobWage}
-								onChangeText={(text) =>
-									setNewData({ ...newData, jobWage: text })
-								}
-							/>
-						</View>
-						<View style={styles.row}>
-							<Text style={styles.subtitle}>Store</Text>
-							<TextInput
-								style={styles.input}
-								placeholder={data.storeName}
-								value={newData.storeName}
-								onChangeText={(text) =>
-									setNewData({ ...newData, storeName: text })
-								}
-							/>
-						</View>
-						<View style={styles.rowLarge}>
-							<Text style={styles.subtitle}>Address</Text>
-							<Text>Street 1</Text>
-							<TextInput
-								style={styles.input}
-								placeholder={data.storeAddress.address_line1}
-								value={newStoreAddress.address_line1}
-								onChangeText={(text) =>
-									setNewStoreAddress({
-										...newStoreAddress,
-										address_line1: text,
-									})
-								}
-							/>
-							<Text>Street 2</Text>
-							<TextInput
-								style={styles.input}
-								placeholder={data.storeAddress.address_line2}
-								value={newStoreAddress.address_line2}
-								onChangeText={(text) =>
-									setNewStoreAddress({
-										...newStoreAddress,
-										address_line2: text,
-									})
-								}
-							/>
-							<Text>City</Text>
-							<TextInput
-								style={styles.input}
-								placeholder={data.storeAddress.city}
-								value={newStoreAddress.city}
-								onChangeText={(text) =>
-									setNewStoreAddress({
-										...newStoreAddress,
-										city: text,
-									})
-								}
-							/>
-							<Text>State</Text>
-							<TextInput
-								style={styles.input}
-								placeholder={data.storeAddress.address_state}
-								value={newStoreAddress.address_state}
-								onChangeText={(text) =>
-									setNewStoreAddress({
-										...newStoreAddress,
-										address_state: text,
-									})
-								}
-							/>
-
-							<Text>Zip</Text>
-							<TextInput
-								style={styles.input}
-								placeholder={data.storeAddress.address_zip}
-								value={newStoreAddress.address_zip}
-								onChangeText={(text) =>
-									setNewStoreAddress({
-										...newStoreAddress,
-										address_zip: text,
-									})
-								}
-							/>
-						</View>
-						<View style={styles.btnContainer}>
-							<TouchableOpacity onPress={handleSubmit} style={styles.btn}>
-								<Text style={styles.btnText}>Save</Text>
-							</TouchableOpacity>
-						</View>
-						{formError !== '' && (
-							<Text style={{ color: `red` }}>{formError}</Text>
-						)}
-						<View style={styles.hardFiller} />
-					</ScrollView>
-				</TouchableWithoutFeedback>
-			</KeyboardAvoidingView>
+			<FormJob 
+				labels={{
+					title: `Edit Job`,
+					button: `Save`
+				}}
+				initFormValue={jobItem 
+					? {
+						jobTitle: jobItem.jobTitle,
+						jobWage: jobItem.jobWage.toString(),
+						storeName: jobItem.storeName,
+						storeAddress: jobItem.storeAddress
+					}
+					: data
+				}
+				onSubmit={(title, wage, name, address) => {
+					editJob(id, title, wage, name, address, () => navigation.pop())
+				}}
+				onDelete={() => {
+					removeJob(id, () => navigation.pop())
+				}}
+				isEdit={true}
+			/>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	hardFiller: {
-		height: Platform.OS === 'ios' ? 100 : 200,
-	},
 	container: {
 		flex: 1,
 		justifyContent: `flex-start`,
 		padding: 12,
-	},
-	title: {
-		fontSize: 36,
-		marginBottom: 10,
-	},
-	subtitle: {
-		fontSize: 16,
 	},
 	header: {
 		flex: 0.1,
@@ -229,50 +89,6 @@ const styles = StyleSheet.create({
 		justifyContent: `center`,
 		alignItems: `flex-start`,
 	},
-	body: {
-		flex: 1,
-	},
-	input: {
-		borderWidth: 1,
-		borderColor: `#A2A7A5`,
-		borderRadius: 5,
-		paddingVertical: 10,
-		paddingHorizontal: 10,
-	},
-	row: {
-		flex: 1,
-	},
-	titleRow: {
-		flex: 1,
-		flexDirection: `row`,
-		justifyContent: `space-between`
-	},
-	rowLarge: {
-		flex: 4,
-	},
-	btnContainer: {
-		marginTop: 12,
-		flex: 1,
-	},
-	btnText: {
-		color: `white`,
-	},
-	btn: {
-		padding: 10,
-		backgroundColor: `#06D6A0`,
-		alignItems: `center`,
-		borderRadius: 2.5,
-	},
-	deleteBtn: {
-		flexDirection: `row`,
-		justifyContent: `flex-end`,
-		alignItems: `center`
-	},
-	deleteText: {
-		color: `#EF476F`,
-		fontSize: 18,
-		marginLeft: 8
-	}
 });
 
 export default EditJob;
